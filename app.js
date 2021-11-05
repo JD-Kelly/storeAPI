a283b16b19b05ec50bb0fcf5e356816c46dd0221
Jonny Kelly <jonny.kelly192@gmail.com>
2021-11-05 17:48:16 +0000

    product routes set-up

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
-
+require('express-async-errors');
 
 
 const express = require('express');
 const app = express();
 const notFoundMiddleware = require('./middleware/not-found');
 const errortMiddleware = require('./middleware/error-handler');
-const { get } = require("mongoose");
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
 
 // middleware
 app.use(express.json())
@@ -16,6 +18,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
 
+app.use('/api/v1/products', productsRouter)
+
 
 app.use(notFoundMiddleware)
 app.use(errortMiddleware)
@@ -24,6 +28,7 @@ app.use(errortMiddleware)
 const port = process.env.PORT || 3000
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, console.log(`server is listening on port ${port}...`))
   } catch (error) {
     console.log(error)
