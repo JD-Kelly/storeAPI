8af140fed0be67a366b2b0c8ec135ea5be3a07d7
Jonny Kelly <jonny.kelly192@gmail.com>
2021-11-09 16:18:39 +0000

    page and limit query functionality added

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price')
+  const products = await Product.find({}).select('name price').limit(4)
   res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -31,6 +31,11 @@ if(fields){
   const fieldsList = fields.split(',').join(' ');
   result = result.select(fieldsList)
 }
+const page = Number(req.query.page) || 1
+const limit = Number(req.query.limit) || 10
+const skip = (page -1) * limit;
+
+result = result.skip(skip).limit(limit)
 const products = await result
 res.status(200).json({ products, nbHits: products.length })
 }
